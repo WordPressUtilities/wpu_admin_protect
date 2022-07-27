6d8641ead738fc280c9ba8ffedbd5267ccd2360b
Kevin <kevin@rocher-dev.com>
2022-07-27 22:08:12 +0200

    v 2.1.3
    - Prevent access to php.ini files

--- wpu_admin_protect.php
+++ wpu_admin_protect.php
@@ -4,7 +4,7 @@
 Plugin Name: WPU Admin Protect
 Plugin URI: https://github.com/WordPressUtilities/wpu_admin_protect
 Description: Restrictive options for WordPress admin
-Version: 2.1.2
+Version: 2.1.3
 Author: Darklg
 Author URI: https://darklg.me/
 License: MIT License
@@ -26,7 +26,7 @@ if (file_exists(ABSPATH . '/.disable_wpu_admin_protect')) {
   Levels
 ---------------------------------------------------------- */
 
-define('WPUTH_ADMIN_PLUGIN_VERSION', '2.1.2');
+define('WPUTH_ADMIN_PLUGIN_VERSION', '2.1.3');
 define('WPUTH_ADMIN_PLUGIN_NAME', 'WPU Admin Protect');
 define('WPUTH_ADMIN_PLUGIN_OPT', 'wpu_admin_protect__v');
 define('WPUTH_ADMIN_MIN_LVL', 'manage_categories');
@@ -290,6 +290,7 @@ function wputh_admin_protect_rewrite_rules($rules) {
         'package-lock\.json$',
         'phpstan\.neon$',
         'composer\.lock$',
+        'php\.ini',
         'phpunit\.xml$',
         /* Infos */
         'changelog\.txt$',
