73ee385b85afded823385990a6810a607e2b109f
Kevin <kevin@rocher-dev.com>
2024-03-20 11:18:48 +0100

    v 3.0.1
    - remove « upload.php » in forbidden files.

--- wpu_admin_protect.php
+++ wpu_admin_protect.php
@@ -5,7 +5,7 @@ Plugin Name: WPU Admin Protect
 Plugin URI: https://github.com/WordPressUtilities/wpu_admin_protect
 Update URI: https://github.com/WordPressUtilities/wpu_admin_protect
 Description: Restrictive options for WordPress admin
-Version: 3.0.0
+Version: 3.0.1
 Author: Darklg
 Author URI: https://darklg.me/
 Text Domain: wpu_admin_protect
@@ -35,7 +35,7 @@ if (defined('DISABLE_WPU_ADMIN_PROTECT') && DISABLE_WPU_ADMIN_PROTECT) {
   Levels
 ---------------------------------------------------------- */
 
-define('WPUTH_ADMIN_PLUGIN_VERSION', '3.0.0');
+define('WPUTH_ADMIN_PLUGIN_VERSION', '3.0.1');
 define('WPUTH_ADMIN_PLUGIN_NAME', 'WPU Admin Protect');
 define('WPUTH_ADMIN_PLUGIN_OPT', 'wpu_admin_protect__v');
 define('WPUTH_ADMIN_MIN_LVL', 'manage_categories');
@@ -355,7 +355,6 @@ function wputh_admin_protect_rewrite_rules($rules) {
         'simple\.php$',
         'system\.php$',
         'timthumb\.php$',
-        'upload\.php$',
         'xleet-shell\.php$',
         'xleet\.php$',
         'xleetshell\.php$',
