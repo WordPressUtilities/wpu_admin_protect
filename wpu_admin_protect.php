153dfbe3e1eafa439dc29f587134c5148d4d3dac
Kevin <kevin@rocher-dev.com>
2025-06-17 18:17:02 +0200

    v 3.2.2
    - fix top wp attacks.

--- wpu_admin_protect.php
+++ wpu_admin_protect.php
@@ -5,7 +5,7 @@ Plugin Name: WPU Admin Protect
 Plugin URI: https://github.com/WordPressUtilities/wpu_admin_protect
 Update URI: https://github.com/WordPressUtilities/wpu_admin_protect
 Description: Restrictive options for WordPress admin
-Version: 3.2.1
+Version: 3.2.2
 Author: Darklg
 Author URI: https://darklg.me/
 Text Domain: wpu_admin_protect
@@ -35,7 +35,7 @@ if (defined('DISABLE_WPU_ADMIN_PROTECT') && DISABLE_WPU_ADMIN_PROTECT) {
   Levels
 ---------------------------------------------------------- */
 
-define('WPUTH_ADMIN_PLUGIN_VERSION', '3.2.1');
+define('WPUTH_ADMIN_PLUGIN_VERSION', '3.2.2');
 define('WPUTH_ADMIN_PLUGIN_NAME', 'WPU Admin Protect');
 define('WPUTH_ADMIN_PLUGIN_OPT', 'wpu_admin_protect__v');
 define('WPUTH_ADMIN_MIN_LVL', 'manage_categories');
@@ -338,15 +338,15 @@ function wputh_admin_protect_rewrite_rules($rules) {
         'README\.md$',
         'readme\.txt$',
         /* WordPress attacks */
-        '/config\.php$',
-        '/chosen\.php$',
-        '/radio\.php$',
-        '/autoload_classmap\.php$',
-        '/file\.php$',
-        '/alfa\.php$',
-        '/cong\.php$',
-        '/wp\.php$',
-        '/error\.php$',
+        'alfa\.php$',
+        'autoload_classmap\.php$',
+        'chosen\.php$',
+        'classwithtostring\.php$',
+        'config\.php$',
+        'cong\.php$',
+        'edit\.php$',
+        'error\.php$',
+        'file\.php$',
         'index_sso\.php$',
         'infophp\.php$',
         'inputs\.php$',
@@ -356,11 +356,13 @@ function wputh_admin_protect_rewrite_rules($rules) {
         'phpinfo\.php$',
         'phpversion\.php$',
         'pinfo\.php$',
+        'radio\.php$',
         's_ne\.php$',
         'shell\.php$',
         'simple\.php$',
         'system\.php$',
         'timthumb\.php$',
+        'wp\.php$',
         'xleet-shell\.php$',
         'xleet\.php$',
         'xleetshell\.php$',
